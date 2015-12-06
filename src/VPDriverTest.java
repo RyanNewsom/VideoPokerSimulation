88abfb28d11d9ddffc5867e9792b9fced96a7605
Ryan <rnewsom@msudenver.edu>
2015-12-06 12:34:04 -0700

    Added design docs

--- src/VPDriverTest.java
+++ src/VPDriverTest.java
@@ -1,16 +1,16 @@
-import junit.framework.TestCase;
-import org.junit.Test;
-
-/**
- * Created by Ryan on 12/5/2015.
- */
-public class VPDriverTest extends TestCase {
-
-    @Test
-    public void testHand(){
-        String payTableString = "800 50 25 8 5 4 3 2 1 0";
-        VPDriver driver = new VPDriver(payTableString);
-        driver.testHand(29, 30, 19, 8, 0);
-    }
-
-}
+//import junit.framework.TestCase;
+//import org.junit.Test;
+//
+///**
+// * Created by Ryan on 12/5/2015.
+// */
+//public class VPDriverTest extends TestCase {
+//
+//    @Test
+//    public void testHand(){
+//        String payTableString = "800 50 25 8 5 4 3 2 1 0";
+//        VPDriver driver = new VPDriver(payTableString);
+//        driver.testHand(29, 30, 19, 8, 0);
+//    }
+//
+//}
